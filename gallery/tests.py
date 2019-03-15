7e6b203103c038302ebd566eadd30ac7a949d45a
umutoni jacqueline <jacquelineumutoni13@gmail.com>
2019-03-15 11:28:35 +0200

    add tests for the modeles

--- gallery/tests.py
+++ gallery/tests.py
@@ -37,64 +37,63 @@ class ImageTestClass(TestCase):
         self.assertEqual(len(images), 1)
 
 
-#------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------
 
 
-# class CategoryTestClass(TestCase):
-#     def setUp(self):
-#         self.travel = Category(name = 'travel')
+class CategoryTestClass(TestCase):
+    def setUp(self):
+        self.travel = Category(name = 'travel')
 
-#     def test_instance(self):
-#         self.assertTrue(isinstance(self.travel, Category))
+    def test_instance(self):
+        self.assertTrue(isinstance(self.travel, Category))
 
-#     def test_save_method(self):
-#         self.travel.save_category()
-#         categories = Category.objects.all()
-#         self.assertTrue(len(categories) > 0)        
+    def test_save_method(self):
+        self.travel.save_category()
+        categories = Category.objects.all()
+        self.assertTrue(len(categories) > 0)        
 
-#     def test_delete_method(self):
-#         self.new_category = Category(name = 'food')
-#         self.new_category.save_category()
-#         self.new_category.delete_category()
-#         categories = Category.objects.all()
-#         self.assertEqual(len(categories), 0)   
+    def test_delete_method(self):
+        self.new_category = Category(name = 'food')
+        self.new_category.save_category()
+        self.new_category.delete_category()
+        categories = Category.objects.all()
+        self.assertEqual(len(categories), 0)   
 
-#     def test_update_category(self):
-#         self.health = Category(name = 'Food')
-#         self.health.save_category()
-#         self.health = Category(name = 'Fashion')
-#         self.health.save_category()
-#         self.health.update_category(name = 'Travel')
-#         categories = Category.objects.filter(name = 'People')
-#         self.assertEqual(len(categories), 1)
+    def test_update_category(self):
+        self.health = Category(name = 'Food')
+        self.health.save_category()
+        self.health = Category(name = 'Fashion')
+        self.health.save_category()
+        self.health.update_category(name = 'Travel')
+        categories = Category.objects.filter(name = 'People')
+        self.assertEqual(len(categories), 1)
 
 
-# #-------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------        
 
-# class LocationTestClass(TestCase):    
-#     def setUp(self):
-#         self.Kenya = Location(name = 'Kenya')
 
-#     def test_instance(self):
-#         self.assertTrue(isinstance(self.Kenya, Location))
+class LocationTestClass(TestCase):    
+    def setUp(self):
+        self.Rwanda = Location(name = ' Rwanda')
 
-#     def test_save_method(self):
-#         self.Kenya.save_location()
-#         locations = Location.objects.all()
-#         self.assertTrue(len(locations) > 0)
+    def test_instance(self):
+        self.assertTrue(isinstance(self.Rwanda, Location))
 
-#     def test_delete_method(self):
-#         self.new_location = Location(name = 'Mombasa')
-#         self.new_location.save_location()
-#         self.new_location.delete_location()
-#         locations = Location.objects.all()
-#         self.assertEqual(len(locations), 0)
+    def test_save_method(self):
+        self.Rwanda.save_location()
+        locations = Location.objects.all()
+        self.assertTrue(len(locations) > 0)
 
-#     def test_update_method(self):
-#         self.Nairobi = Location(name = 'Mombasa')
-#         self.Nairobi.save_location()
-#         self.Nairobi = Location(name = 'Nairobi')
-#         self.Nairobi.save_location()
-#         self.Nairobi.update_location(name = 'Nairobi')
-#         locations = Location.objects.filter(name = 'Nairobi')
-#         self.assertEqual(len(locations), 1)   
+    def test_delete_method(self):
+        self.new_location = Location(name = 'Rubavu')
+        self.new_location.save_location()
+        self.new_location.delete_location()
+        locations = Location.objects.all()
+        self.assertEqual(len(locations), 0)
+
+    def test_update_method(self):
+        self.Kigali = Location(name = 'Rubavu')
+        self.Kigali.save_location()
+        self.Kigali = Location(name = 'Kigali')
+        self.Kigali.save_location()
+        self.Kigali.update_location(name = 'Kigali')
+        locations = Location.objects.filter(name = 'Kigali')
+        self.assertEqual(len(locations), 1)   
